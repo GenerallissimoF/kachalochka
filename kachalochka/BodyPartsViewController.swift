17dfbe5641b125f73de95860e20b72ab4cdfce3e
GenerallissimoF <95148081+GenerallissimoF@users.noreply.github.com>
2022-01-07 01:11:29 +0300

    HW 7 commit 5

--- kachalochka/BodyPartsViewController.swift
+++ kachalochka/BodyPartsViewController.swift
@@ -7,44 +7,45 @@
 
 import UIKit
 
-class ExersiseTableViewController: UITableViewController {
+class BodyPartsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-   
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
         
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-  
+      
         var content = cell.defaultContentConfiguration()
         switch indexPath.row {
         case 0: content.text = "Chest"
         case 1: content.text = "Legs"
         default: content.text = "Back"
         }
+    
         cell.contentConfiguration = content
+       
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
       
         if indexPath.row == 0 {
             performSegue(withIdentifier: "seg", sender: self)
